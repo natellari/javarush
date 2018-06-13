18adebdf8b5a7c88e7fd62ce5b357b97d04d4b48
natellari <natella.ri@gmail.com>
2018-06-13 12:22:24 +0300

    delete cached

--- 2.JavaCore/src/com/javarush/task/task13/task1328/Robot.java
+++
@@ -1,9 +0,0 @@
-package com.javarush.task.task13.task1328;
-
-public class Robot extends AbstractRobot implements Attackable, Defensable {
-
-
-    public Robot(String name) {
-        super(name);
-    }
-}
--- 2.JavaCore/src/com/javarush/task/task14/task1409/Bridge.java
+++
@@ -1,8 +0,0 @@
-package com.javarush.task.task14.task1409;
-
-/**
- * Created by n.yarceva on 29.05.2018.
- */
-public interface Bridge {
-    int getCarsCount();
-}
